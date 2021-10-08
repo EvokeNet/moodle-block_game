781f4f06d17b81a77158f24942d778ed0176e7e2
Willian Mano <willianmanoarajo@gmail.com>
2021-10-08 14:19:12 -0300

    Fixes lang typo

--- lang/en/block_game.php
+++ lang/en/block_game.php
@@ -216,4 +216,4 @@ $string['downloadthis'] = 'Download as';
 $string['evocoins'] = 'Evocoins';
 $string['collectedsuperpowers'] = 'Collected superpowers';
 $string['selectyouragentavatar'] = 'Select your agent avatar:';
-$string['viewscoreboard'] = 'View score board';
+$string['viewscoreboard'] = 'View scoreboard';
